4cabc2493d913707d6d246f1a1ea6814e338a655
NoSpectators <john.c.erickson@gmail.com>
2025-11-24 20:33:14 -0500

    egg complete

---
+++ egg.f95
@@ -0,0 +1,17 @@
+! write a complete program that displays the following output:
+!    _______
+!   /       \ 
+!  /         \ 
+!  -"-'-"-'-"-
+!  \         / 
+!   \_______/
+
+program egg
+    implicit none
+    print *, "  _______"
+    print *, " /       \"
+    print *, "/         \"
+    print *, "-""-'-""-'-""-"
+    print *, "\         /"
+    print *, " \_______/"
+end program egg
