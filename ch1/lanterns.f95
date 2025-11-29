da21bf6a8670d9f8056787dcadb455ff9ac9bcda
NoSpectators <john.c.erickson@gmail.com>
2025-11-29 16:34:46 -0500

    works

--- ch1/lanterns.f95
+++ ch1/lanterns.f95
@@ -29,6 +29,18 @@
 program Lanterns
     implicit none
     call cone()    
+    call printSpace()
+    call cone()
+    call bars()
+    call lineAsterisks()
+    call cone()
+    call printSpace()
+    call cone()
+    call fiveAsterisks()
+    call bars()
+    call bars()
+    call fiveAsterisks()
+    call fiveAsterisks()
 end program Lanterns
 
 subroutine cone() 
@@ -36,6 +48,22 @@ subroutine cone()
     integer :: i
     do i = 1, 3
         write(*, '(A)', advance='no') repeat(' ', -2 * i + 6) !leading spaces
-        write(*, '(A)'), repeat('*', 4 * i + 1) !asterisks
+        write(*, '(A)') repeat('*', 4 * i + 1) !asterisks
     enddo
 end subroutine cone
+
+subroutine printSpace()
+    write(*,*)
+end subroutine printSpace
+
+subroutine bars()
+    write(*,'(A)') "* | | | | | *"
+end subroutine bars
+
+subroutine lineAsterisks()
+    write(*, '(A)') "*************"
+end subroutine lineAsterisks
+
+subroutine fiveAsterisks()
+    write(*, '(A)') "    *****    "
+end subroutine fiveAsterisks
