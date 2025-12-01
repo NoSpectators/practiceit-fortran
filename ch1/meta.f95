7bc7ac7fe1e9b65df14b74e688557e9edf7b6636
NoSpectators <john.c.erickson@gmail.com>
2025-12-01 09:43:15 -0500

    meta

---
+++ ch1/meta.f95
@@ -0,0 +1,11 @@
+! Write a complete Fortran program called meta_writer whose output is the text
+
+! program HelloWorld
+!   implicit none 
+!   print *, "Hello, world!" 
+! end program HelloWorld
+
+program HelloWorld
+    implicit none
+    print *, "Hello, world!"
+end program HelloWorld
