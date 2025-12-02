b5f915f3c289847b6e224015a4c77fc7526e06e0
NoSpectators <john.c.erickson@gmail.com>
2025-12-02 14:58:47 -0500

    spikey

---
+++ ch1/spikey.f95
@@ -0,0 +1,18 @@
+! Write a complete program in a class named Spikey that prints the 
+! following output:
+!   \/
+!  \\//
+! \\\///
+! ///\\\
+!  //\\
+!   /\
+
+program Spikey
+    implicit none
+    write(*,*) "  \/  "
+    write(*,*) " \\// "
+    write(*,*) "\\\///"
+    write(*,*) "///\\\"
+    write(*,*) " //\\ "
+    write(*,*) "  /\  "
+end program Spikey
