fdb2aa2cbb8f413d34a4bf8cdc00299f17a142e9
NoSpectators <john.c.erickson@gmail.com>
2025-12-04 20:38:36 -0500

    stewie

---
+++ ch1/stewie.f95
@@ -0,0 +1,11 @@
+! Write a complete Java program in a class named Stewie that prints the
+! following output:
+! //////////////////////
+! || Victory is mine! ||
+! \\\\\\\\\\\\\\\\\\\\\\
+
+program Stewie
+    write(*,*) "//////////////////////"
+    write(*,*) "|| Victory is mine! ||"
+    write(*,*) "\\\\\\\\\\\\\\\\\\\\\\"
+end program Stewie
